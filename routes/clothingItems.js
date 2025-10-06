89979275c13555a8c12f8f12fc525de0ae97fbda
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-10-05 23:46:27 -0500

    fix likes routes

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const { getItems, createItem, getItem, deleteItem } = require('../controllers/clothingItems');
+const { likeItem, dislikeItem } = require('../controllers/likes');
 
 router.get('/', getItems);
 router.get('/:itemId', getItem);
 router.post('/', createItem);
 router.delete('/:itemId', deleteItem )
+
+router.put('/:itemId/likes', likeItem);
+router.delete('/:itemId/likes', dislikeItem);
 
 module.exports = router;
