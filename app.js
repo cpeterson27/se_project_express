df00b71c03e3c4ab7bb8943e3889b684ef74b105
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-10-13 14:20:51 -0500

    add comma after _id removed empty lines added semicolons added export for app

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 const mainRouter = require("./routes/index");
-const likesRouter = require("./routes/likes")
+const likesRouter = require("./routes/likes");
 const { createUser } = require("./controllers/users");
 const { login } = require("./controllers/users");
 const auth = require("./middlewares/auth");
-
 const { getItems } = require("./controllers/clothingItems");
 
 const { PORT = 3001 } = process.env;
@@ -15,30 +14,27 @@ const app = express();
 mongoose.set("strictQuery", false);
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {
-  })
+  .then(() => {})
   .catch(console.error);
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "5d8b8592978f8bd833ca8133"
+    _id: "5d8b8592978f8bd833ca8133",
   };
   next();
 });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
-
-app.get('/items', getItems)
+app.post("/signin", login);
+app.post("/signup", createUser);
+app.get("/items", getItems);
 
 app.use(auth);
-
 app.use(likesRouter);
-
 app.use("/", mainRouter);
 
-app.listen(PORT, () => {
-});
+app.listen(PORT, () => {});
+
+module.exports = app;
